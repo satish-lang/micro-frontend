9e5662cfed0cbec3800d2c16eb4edee9e3e3ada9
satish <satish@fleetx.io>
2025-08-17 21:23:56 +0530

    fix css

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { Switch, Route } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core/styles";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: "ma",
+});
+
 export default function App() {
   return (
-    <StylesProvider>
+    <StylesProvider generateClassName={generateClassName}>
       <BrowserRouter>
         <Switch>
           <Route path="/" exact component={Landing} />
